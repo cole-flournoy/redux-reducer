7a5932f833bce3ea331a05daf973d3eb9ed3179c
Cole Flournoy <flournoy.cole@gmail.com>
2021-05-24 17:21:59 -0400

    commit tree a9eca69e50464bda8707c3b808845556d1f1e4ee

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+  if (action.type === "ADD_FRIEND"){
+    let newState = {friends: [...state.friends, action.friend]}
+    return newState
+  } else if (action.type === "REMOVE_FRIEND"){
+    let newState = {friends: state.friends.filter(e => e.id !== action.id)}
+    return newState
+  } else {
+    return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
-export function managePresents(state, action){
+export function managePresents(state={numberOfPresents: 0}, action){
+  if (action.type === "INCREASE"){
+    let newState = {numberOfPresents: state.numberOfPresents + 1}
+    return newState
+  } else {
+    return state
+  }
 }
